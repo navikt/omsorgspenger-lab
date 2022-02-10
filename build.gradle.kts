524abdb2e7b8f809fe7d43f42a996b96400527e3
albrektsson <emil.albrektsson@nav.no>
2022-02-10 13:35:08 +0100

    Lagt til dockerfil & shadowjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-	id("org.springframework.boot") version "2.6.3"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "no.nav.omsorgspenger"
@@ -12,41 +14,59 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
-	maven("https://jitpack.io")
+    mavenCentral()
+    maven("https://jitpack.io")
 }
 
 extra["azureVersion"] = "3.13.0"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-	implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
 
-	implementation("com.azure.spring:azure-spring-boot-starter-active-directory")
+    implementation("com.azure.spring:azure-spring-boot-starter-active-directory")
 
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("com.azure.spring:azure-spring-boot-bom:${property("azureVersion")}")
-	}
+    imports {
+        mavenBom("com.azure.spring:azure-spring-boot-bom:${property("azureVersion")}")
+    }
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
-}
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    withType<ShadowJar> {
+        archiveBaseName.set("app")
+        archiveClassifier.set("")
+        manifest {
+            attributes(
+                mapOf(
+                    "Main-Class" to "no.nav.omsorgspenger.OmsorgspengerLabApplicationKt"
+                )
+            )
+        }
+    }
+
+    withType<Wrapper> {
+        gradleVersion = "7.4"
+    }
 }
